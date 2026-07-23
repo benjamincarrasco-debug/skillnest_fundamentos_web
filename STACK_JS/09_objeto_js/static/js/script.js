b99ed2c996de13cea5a5f7f5155f9d9b868d7388
benjamin carrasco <benjamincarrasco@liceovvh.cl>
2026-07-23 12:59:18 -0400

    m

---
+++ STACK_JS/09_objeto_js/static/js/script.js
@@ -0,0 +1,71 @@
+console.log(`hola mundo`)
+
+let hamburguesaEspecial = {
+
+    pan: "Pan brioche",
+
+    carne: "Pollo crujiente",
+
+    queso: "Suizo",
+
+    extras: [
+
+        "Lechuga",
+
+        "Pepinos",
+
+        "Miel"
+
+    ],
+
+    mostrarIngredientes: function () {
+
+        console.log("Pan:", this.pan);
+
+        console.log("Carne:", this.carne);
+
+        console.log("Queso:", this.queso);
+
+        console.log(
+
+            "Extras:",
+
+            this.extras.join(", ")
+
+        );
+
+    }
+console.log(hamburguesaEspecial.pan);
+hamburguesaEspecial.mostrarIngredientes();
+};
+
+
+let hamburguesaVegana = {
+
+    pan:"Integral",
+
+    carne:"Proteína vegetal",
+
+    queso:"Vegano",
+
+    extras:[
+
+        "Tomate",
+
+        "Rúcula"
+
+    ],
+
+    mostrarIngredientes:function(){
+
+        console.log(this.pan);
+
+        console.log(this.carne);
+
+        console.log(this.queso);
+
+    }
+console.log(hamburguesaEspecial.mostrarIngredientes);
+
+hamburguesaVegana.mostrarIngredientes();
+};
