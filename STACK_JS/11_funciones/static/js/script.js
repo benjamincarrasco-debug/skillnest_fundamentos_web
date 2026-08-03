23202c2630f625a3606847aca904f9dc868d9442
benjamin carrasco <benjamincarrasco@liceovvh.cl>
2026-08-03 08:20:07 -0400

    s
    s

--- STACK_JS/11_funciones/static/js/script.js
+++ STACK_JS/11_funciones/static/js/script.js
@@ -27,6 +27,25 @@ alert("El número mayor entre", numero1, "y", numero2, "es:", maximo);
 //tarea
 //crear una funcion que reciba 3 parametros, a ,b y c, debe sumar a+ b
 //  y el restarlo po c.devolver el valor final y mostrar con un alert.}
-function calcular(){
+function operaciones(a, b, c){
+        return a + b + c;
+}
+let num1 = parseInt(promt("Ingrese primer numero"))
+let num2 = parseInt(promt("Ingrese segundo numero"))
+let num3 = parseInt(promt("Ingrese tercer numero"))
+let resultado = operaciones()
+alert(`La operación de ${num1} + ${num2} - ${num3} = ${resultado}`)
 
+let parametro = parseInt(prompt(`ingrede el limite del contador`));
+function contadorNumeros(a){
+    let numeros = []
+    for(let i=0;i<=0;i++){
+        numeros.push(i)
+    }
+}
+
+function mostrarConteo(){
+    let parametro = parseInt(prompt(`ingrese el limite del contador`));
+    if(parametro <=100){alert(`el conteo es ${resultado}`)}
+    else{alert(`el`)}
 }
